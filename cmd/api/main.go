754e14165563bffd19ec165becdb8533b2324bbb
faridMeli <farid.ahmed@mercadolibre.com>
2022-09-02 16:42:24 -0300

    uncompress files

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"compress/gzip"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"strings"
 	"sync"
@@ -29,7 +31,8 @@ func main() {
 	var dump string = "Brick"
 	var lines []data.DataCompressed
 
-	files := listDirByReadDir("/Users/farahmed/Downloads/Bricks")
+	files := listDirByReadDir("/Users/farahmed/Downloads/Layouts")
+	uncompressAndClearDirectory(&files)
 	list := make(chan data.DataCompressed)
 
 	go readChannel(list, &lines)
@@ -51,6 +54,63 @@ func main() {
 		executor.RevoverDump()
 	}
 
+}
+
+func uncompressGzFile(name string) (string, error) {
+
+	if !strings.Contains(name, ".gz") {
+		return "", nil
+	}
+	// Open compressed file
+	gzipFile, err := os.Open(name)
+	if err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+
+	// Create a gzip reader on top of the file reader
+	// Again, it could be any type reader though
+	gzipReader, err := gzip.NewReader(gzipFile)
+	if err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+	defer gzipReader.Close()
+
+	// Uncompress to a writer. We'll use a file writer
+	newName := strings.TrimSuffix(name, ".gz")
+	outfileWriter, err := os.Create(newName)
+
+	if err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+	defer outfileWriter.Close()
+
+	// Copy contents of gzipped file to output file
+	_, err = io.Copy(outfileWriter, gzipReader)
+	if err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+
+	err = os.Remove(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return newName, nil
+}
+
+func uncompressAndClearDirectory(directory *[]string) {
+	for _, file := range *directory {
+		name, err := uncompressGzFile(file)
+		if err != nil {
+			log.Fatal("Failed")
+		}
+		if name != "" {
+			*directory = append(*directory, name)
+		}
+	}
 }
 
 func readChannel(list chan data.DataCompressed, lines *[]data.DataCompressed) {
